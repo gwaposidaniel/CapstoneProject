317a294b5d31724f17534785ccf1dc04f0b2cddb
unknown <danp.salvacion.ui@phinmaed.com>
2024-02-19 23:01:13 +1200

    change name

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
   <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.php">FINDIT<span>
+      <h1 class="logo"><a href="index.php">Daniel salvacion<span>
             <div class=""></div>
           </span></a></h1>
       <nav id="navbar" class="navbar">
